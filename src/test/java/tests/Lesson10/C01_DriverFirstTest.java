7be4423d92229dc0455e604aaec7ff0d19d7505e
alitu <alym11731996@gmail.com>
2024-02-23 01:16:35 +0300

    Driver first test

---
+++ src/test/java/tests/Lesson10/C01_DriverFirstTest.java
@@ -0,0 +1,17 @@
+package tests.Lesson10;
+
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C01_DriverFirstTest {
+   @Test
+
+    public void test() {
+       Driver.getDriver().get("https://www.amazon.com.tr");
+       Driver.getDriver().get("https://www.bestbuy.com");
+       Driver.closeDriver();
+       Driver.getDriver().get("https://www.hepsiburada.com");
+       Driver.closeDriver();
+    }
+}
